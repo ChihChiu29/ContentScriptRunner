8d4ed1f80d1020f2b3e1273500e08e5fefecdad9
Chih Chiu <chih.chiu.19@gmail.com>
2018-04-14 21:17:10 -0700

    try again

--- hosted_scripts/dnvod.js
+++ hosted_scripts/dnvod.js
@@ -1,10 +1,10 @@
 // Remove ads from https://www.dnvod.tv/
 
 whenDOMIsStable(function() {
-  // Homepage.
-  //$('#_LU').remove();
-  //$('.i-cp, .gsd').remove();
-  // Play page.
-  //$('.bfq-l-2').get();
-  //$('.main, .intro').remove();
+  // Banner ads
+  $('.i-cp').remove();
+  // Floating bottom-right
+  $('.aa').remove();
+  // Play window; side ads.
+  $('.bfq-l-2').remove();
 });
